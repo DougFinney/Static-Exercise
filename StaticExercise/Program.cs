cd393343a5b96ddd0d6b618e86ed7394e0ba38c1
Doug Finney <dugfiny@gmail.com>
2023-01-12 17:59:11 -0600

    Static Temp Converter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is the temperature in F you want to convert?");
+            var fahTemp = Convert.ToDouble(Console.ReadLine()); 
+            var convertToCel = TempConverter.FahrenheitToCelsius(fahTemp);
+
+            Console.WriteLine($"that equals {convertToCel} degress Celsius");
+            Console.WriteLine();
+
+            Console.WriteLine("what is temperature in C you want to convert?");
+            var celTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"That equals {TempConverter.CelsiusToFahrenheit(celTemp)} degrees F");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            var celTemp = (fTemp - 32) * (5.0 / 9.0);
+            return celTemp;
+        }
+        public static double CelsiusToFahrenheit(double ctemp) 
+        { 
+            var fahTemp = (ctemp * (9d / 5d)) + 32;
+            return fahTemp;
+        }
+    }
+}
